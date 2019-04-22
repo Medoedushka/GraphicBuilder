5a5f95870f238317c2a72deff746fcadb3456296
Medoedushka <knife491@gmail.com>
2019-04-22 03:43:39 +0300

    РќР°С‡Р°Р»СЊРЅРѕРµ РїРѕСЃС‚СЂРѕРµРЅРёРµ СЂС‚-РіСЂР°С„РёРєР°

--- GraphicBuilder/RT_Graphic.Designer.cs
+++ GraphicBuilder/RT_Graphic.Designer.cs
@@ -29,12 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.lbl_Status = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txb_FilePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,6 +48,7 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(143)))), ((int)(((byte)(173)))));
             this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btn_Connect);
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Controls.Add(this.label1);
@@ -51,15 +58,87 @@
             this.panel1.Size = new System.Drawing.Size(273, 557);
             this.panel1.TabIndex = 0;
             // 
-            // pictureBox1
+            // btn_Connect
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(711, 557);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.btn_Connect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_Connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Connect.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Connect.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
+            this.btn_Connect.Location = new System.Drawing.Point(6, 511);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(118, 34);
+            this.btn_Connect.TabIndex = 3;
+            this.btn_Connect.Text = "Подключиться";
+            this.btn_Connect.UseVisualStyleBackColor = true;
+            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(6, 213);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 209);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "USB-порт";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_OpenFile);
+            this.groupBox1.Controls.Add(this.lbl_Status);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txb_FilePath);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(6, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 105);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Текстовый файл";
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btn_OpenFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_OpenFile.Font = new System.Drawing.Font("Arial", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_OpenFile.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
+            this.btn_OpenFile.Location = new System.Drawing.Point(12, 64);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(106, 23);
+            this.btn_OpenFile.TabIndex = 4;
+            this.btn_OpenFile.Text = "Открыть файл";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click);
+            // 
+            // lbl_Status
+            // 
+            this.lbl_Status.AutoSize = true;
+            this.lbl_Status.Font = new System.Drawing.Font("Wingdings 2", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.lbl_Status.ForeColor = System.Drawing.Color.Red;
+            this.lbl_Status.Location = new System.Drawing.Point(124, 67);
+            this.lbl_Status.Name = "lbl_Status";
+            this.lbl_Status.Size = new System.Drawing.Size(24, 21);
+            this.lbl_Status.TabIndex = 2;
+            this.lbl_Status.Text = "O";
+            this.lbl_Status.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Путь к файлу:";
+            // 
+            // txb_FilePath
+            // 
+            this.txb_FilePath.Enabled = false;
+            this.txb_FilePath.Location = new System.Drawing.Point(12, 40);
+            this.txb_FilePath.Name = "txb_FilePath";
+            this.txb_FilePath.ReadOnly = true;
+            this.txb_FilePath.Size = new System.Drawing.Size(243, 22);
+            this.txb_FilePath.TabIndex = 0;
             // 
             // label1
             // 
@@ -72,38 +151,25 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Настройки";
             // 
-            // groupBox1
+            // pictureBox1
             // 
-            this.groupBox1.ForeColor = System.Drawing.Color.White;
-            this.groupBox1.Location = new System.Drawing.Point(24, 57);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(237, 135);
-            this.groupBox1.TabIndex = 1;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Текстовый файл";
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.ForeColor = System.Drawing.Color.White;
-            this.groupBox2.Location = new System.Drawing.Point(24, 213);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(237, 209);
-            this.groupBox2.TabIndex = 2;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "USB-порт";
+            this.pictureBox1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(711, 557);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(235)))), ((int)(((byte)(35)))), ((int)(((byte)(50)))));
-            this.button1.Location = new System.Drawing.Point(6, 511);
+            this.button1.Location = new System.Drawing.Point(143, 517);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(118, 34);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Подключиться";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // RT_Graphic
             // 
@@ -119,8 +185,11 @@
             this.Name = "RT_Graphic";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Построение графика в реальном времени";
+            this.Load += new System.EventHandler(this.RT_Graphic_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -130,9 +199,14 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Connect;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_Status;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txb_FilePath;
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.Button button1;
     }
 }
--- GraphicBuilder/RT_Graphic.cs
+++ GraphicBuilder/RT_Graphic.cs
@@ -8,21 +8,110 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MyDrawing;
+using System.IO;
 
 
 namespace GraphicBuilder
 {
     public partial class RT_Graphic : Form
     {
-       
-        
+        private delegate void DrawDel();
+        PointsGraphic RTgraph;
+        PointF[] RTPoints = new PointF[Int16.MaxValue];
+        public List<double> YValues = new List<double>();
+        int Time { get; set; }
+        public string PathTxt { get; set; }
+        bool ActiveConnection { get; set; }
+
         public RT_Graphic()
         {
             InitializeComponent();
             
         }
 
+        private void RT_Graphic_Load(object sender, EventArgs e)
+        {
+            RTgraph = new PointsGraphic(pictureBox1, AxesMode.Static, AxesPosition.AllQuarters);
+            RTgraph.Config.Grid = true;
+            //RTgraph.Config.SmoothAngles = true;
+            RTgraph.Config.PriceForPointOX = 1;
+            RTgraph.Config.PriceForPointOY = 1;
+            
+            lbl_Status.Visible = true;
+            ActiveConnection = false;
+            
+        }
 
-        
+        private void btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            string pathFile; DialogResult result;
+
+            using (OpenFileDialog openFile = new OpenFileDialog())
+            {
+                result = openFile.ShowDialog();
+                pathFile = openFile.FileName;
+            }
+
+            if (result == DialogResult.OK)
+            {
+                //if (File.ReadAllText(pathFile) == string.Empty && Path.GetExtension(pathFile) == ".txt")
+                //{
+                    lbl_Status.ForeColor = Color.Green;
+                    lbl_Status.Text = "P";
+                    txb_FilePath.Text = pathFile;
+                    PathTxt = pathFile;
+                //}
+                //else
+                //{
+                //    lbl_Status.ForeColor = Color.Red;
+                //    lbl_Status.Text = "O";
+                //}
+            }
+        }
+
+        private void DrawRTGraph()
+        {
+            Time = 0;
+            while (ActiveConnection)
+            {
+                FileStream input = new FileStream(PathTxt, FileMode.Open, FileAccess.Read, FileShare.ReadWrite | FileShare.Delete);
+                using (StreamReader reader = new StreamReader(input))
+                {
+                    string[] el = reader.ReadToEnd().Split(' ');
+                    double value = double.Parse(el[el.Length - 2]);
+                    YValues.Add(value);
+                    double x = RTgraph.Center.X - 1 * RTgraph.Config.StepOX / RTgraph.Config.PriceForPointOX;
+                    
+                    RTgraph.Center = new Point((int)x, RTgraph.Center.Y);
+                    
+                    RTgraph.DrawRTGraph(YValues, Time);
+                    Time++;
+                    Thread.Sleep(50);
+                }
+            }
+        }
+
+        private async Task DrawRTGraphAsync()
+        {
+            await Task.Run(() => DrawRTGraph());
+            MessageBox.Show("Done!");
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            ActiveConnection = true;
+            RTgraph.DrawAxes();
+            Thread.Sleep(1000);
+            DrawRTGraphAsync();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ActiveConnection = false;
+            RTPoints = null;
+            GC.Collect(2);
+        }
     }
 }
